b815b854598755e9c1d0a79f0c67e65f61f21b2c
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-02-04 19:03:21 +0100

    Add CI testing (#1)

--- include/libcred.hpp
+++ include/libcred.hpp
@@ -4,51 +4,53 @@
 #include <string>
 #include <vector>
 
-namespace libcred {
+namespace libcred
+{
 
-typedef std::pair<std::string, std::string> Credentials;
+    typedef std::pair<std::string, std::string> Credentials;
 
-enum LIBCRED_RESULT {
-  SUCCESS,
-  FAIL_ERROR,
-  FAIL_NONFATAL
-};
+    enum LIBCRED_RESULT
+    {
+        SUCCESS,
+        FAIL_ERROR,
+        FAIL_NONFATAL
+    };
 
 #ifdef _WIN32
-    #ifdef LIBCRED_STATIC_LIB
-        #define LIBCRED_PUBLIC_API
-    #else
-        #ifdef LIBCRED_EXPORTS
-            #define LIBCRED_PUBLIC_API __declspec(dllexport)
-        #else
-            #define LIBCRED_PUBLIC_API __declspec(dllimport)
-        #endif
-    #endif
+#ifdef LIBCRED_STATIC_LIB
+#define LIBCRED_PUBLIC_API
 #else
-  #define LIBCRED_PUBLIC_API
+#ifdef LIBCRED_EXPORTS
+#define LIBCRED_PUBLIC_API __declspec(dllexport)
+#else
+#define LIBCRED_PUBLIC_API __declspec(dllimport)
+#endif
+#endif
+#else
+#define LIBCRED_PUBLIC_API
 #endif
 
-LIBCRED_PUBLIC_API LIBCRED_RESULT SetPassword(const std::string& service,
-                             const std::string& account,
-                             const std::string& password,
-                             std::string* error);
+    LIBCRED_PUBLIC_API LIBCRED_RESULT SetPassword(const std::string& service,
+                                                  const std::string& account,
+                                                  const std::string& password,
+                                                  std::string* error);
 
-LIBCRED_PUBLIC_API LIBCRED_RESULT GetPassword(const std::string& service,
-                             const std::string& account,
-                             std::string* password,
-                             std::string* error);
+    LIBCRED_PUBLIC_API LIBCRED_RESULT GetPassword(const std::string& service,
+                                                  const std::string& account,
+                                                  std::string* password,
+                                                  std::string* error);
 
-LIBCRED_PUBLIC_API LIBCRED_RESULT DeletePassword(const std::string& service,
-                                const std::string& account,
-                                std::string* error);
+    LIBCRED_PUBLIC_API LIBCRED_RESULT DeletePassword(const std::string& service,
+                                                     const std::string& account,
+                                                     std::string* error);
 
-LIBCRED_PUBLIC_API LIBCRED_RESULT FindPassword(const std::string& service,
-                              std::string* password,
-                              std::string* error);
+    LIBCRED_PUBLIC_API LIBCRED_RESULT FindPassword(const std::string& service,
+                                                   std::string* password,
+                                                   std::string* error);
 
-LIBCRED_PUBLIC_API LIBCRED_RESULT FindCredentials(const std::string& service,
-                                 std::vector<Credentials>*,
-                                 std::string* error);
+    LIBCRED_PUBLIC_API LIBCRED_RESULT FindCredentials(const std::string& service,
+                                                      std::vector<Credentials>*,
+                                                      std::string* error);
 
 }  // namespace keytar
 
